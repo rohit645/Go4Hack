4694907984a9aab162cb0751643fec6a048dba2e
mannat-kapil <mannat050299@gmail.com>
2018-02-02 23:31:10 -0800

    Fireball added

--- Save the world.py
+++ Save the world.py
@@ -28,5 +28,5 @@ while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            sys.quit()
+            quit()
     pygame.display.update()
