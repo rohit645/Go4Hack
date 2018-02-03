942929e2014ce5e2ff3ea774033cb87baa3c1e75
Chirag Thakur <chirag11032000@gmail.com>
2018-02-03 07:32:18 -0800

    Flames and fireballs added

--- Save the world.py
+++ Save the world.py
@@ -5,28 +5,73 @@ Created on Sat Feb  3 11:29:38 2018
 @author: rohit
 """
 
-import pygame,time,random,sys
+import pygame, time, random, sys
 from pygame.locals import *
 
 pygame.init()
 
-RESOLUTION = (800,600)
+display_height, display_width = 600, 1000
+RESOLUTION = (display_width, display_height)
 DISPLAYSURF = pygame.display.set_mode(RESOLUTION)
-pygame.display.set_caption("Save The World!")
+pygame.display.set_caption("Save the world!")
+CLOCK = pygame.time.Clock()
+BLACK = (0, 0, 0)
 
-# define colors
-BLACK = (0,0,0)
-WHITE = (255,255,255)
+class flames:
+    def __init__(self):
+        self.image = load_image("images/flames.png")
+        self.width, self.height = 100, 600
+        self.position = (display_width - self.width, 0)
 
-#add menu screen
+class fireball:
+    global velocity
+    velocity = 5
 
+    def __init__(self):
+        self.image = load_image("images/fireball.png")
+        self.width, self.height = 104, 100
+        self.position_x = display_width
+        self.position_y = random.randint(0, display_height)
 
-#main game loop
-# anything after the game has started is written inside this loop 
-while True:
-    DISPLAYSURF.fill(BLACK)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            quit()
-    pygame.display.update()
+    def update(self):
+        self.position_x -= velocity
+
+def load_image(name):
+     return pygame.image.load(name)
+
+def create_fireballs():
+    fireballs = []
+    for i in range(4):
+        fireballs.append(fireball())
+        fireballs[i].position_x += i * (display_width / 4)
+    return fireballs
+
+def game_loop():
+    game_exit = False
+    firewall = flames()
+    fireballs = create_fireballs()
+
+    while not game_exit:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                game_exit = True
+                break
+
+        DISPLAYSURF.fill(BLACK)
+
+        for i in range(4):
+            fireballs[i].update()
+
+            if fireballs[i].position_x <= -fireballs[i].width:
+                fireballs[i].position_x += display_width
+                fireballs[i].position_y = random.randint(0, display_height)
+
+            DISPLAYSURF.blit(fireballs[i].image, (fireballs[i].position_x, fireballs[i].position_y))
+
+        DISPLAYSURF.blit(firewall.image, firewall.position)
+        pygame.display.update()
+        CLOCK.tick(60)
+
+game_loop()
+pygame.quit()
+quit()
