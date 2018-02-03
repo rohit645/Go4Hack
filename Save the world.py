89cf45118558465347db5d78d608e664b9306ebb
rohithill <torohithill@gmail.com>
2018-02-03 11:39:53 +0530

    Created basic template
    Defined window size with color codes.

---
+++ Save the world.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  3 11:29:38 2018
+
+@author: rohit
+"""
+
+import pygame,time,random,sys
+from pygame.locals import *
+
+pygame.init()
+
+RESOLUTION = (800,600)
+DISPLAYSURF = pygame.display.set_mode(RESOLUTION)
+pygame.display.set_caption("Save The World!")
+
+# define colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+#add menu screen
+
+
+#main game loop
+# anything after the game has started is written inside this loop 
+while True:
+    DISPLAYSURF.fill(BLACK)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.quit()
+    pygame.display.update()
