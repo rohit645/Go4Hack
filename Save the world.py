daeb666f7309b5436d6cf95fe2e8a4069a3b51cc
Lakshay Gupta <lakshaygpt28@gmail.com>
2018-02-04 10:43:19 +0530

    sound added

--- Save the world.py
+++ Save the world.py
@@ -19,6 +19,7 @@ BLACK = (0,0,0)
 WHITE = (255,255,255)
 
 #add menu screen
+BGCOLOR = BLACK #Background color
 
 
 #main game loop
@@ -142,9 +143,72 @@ def gameloop():
             jet.crash()
         for i in range(4):
             if fireballs[i].position_y + fireballs[i].height - 30 > y :
-                    if not (x >= fireballs[i].position_x + fireballs[i].width or x + jet.width <= fireballs[i].position_x -25) :
+                    if not (x >= fireballs[i].position_x + fireballs[i].width -25 or x + jet.width <= fireballs[i].position_x -25) :
                         jet.crash()
                     #if (x + jet.width >= fireballs[i].position_x + 25 and x + jet.width <= fireballs[i].position_x + fireballs[i].width -  25):
         pygame.display.update()
         clock.tick(60)
-gameloop()
+
+# main game loop
+# anything after the game has started is written inside this loop
+def main():
+    global DISPLAYSURF, FPSCLOCK, IMAGESDICT, BASICFONT
+
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+
+    DISPLAYSURF = pygame.display.set_mode(RESOLUTION)
+
+    pygame.display.set_caption("Save The World!")
+    BASICFONT = pygame.font.Font("freesansbold.ttf",22)
+
+    IMAGESDICT = {'title':pygame.image.load('images/title_image.png')}
+
+    startScreen()
+    gameloop()
+    terminate()
+
+    while True:
+        DISPLAYSURF.fill(BLACK)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+        pygame.display.update()
+
+def startScreen():
+    titleRect = IMAGESDICT['title'].get_rect()
+    topCoord = RESOLUTION[1]//2 - titleRect.height
+    titleRect.top = topCoord
+    titleRect.centerx = RESOLUTION[0]//2
+    topCoord+=titleRect.height
+
+    DISPLAYSURF.fill(BGCOLOR)
+    DISPLAYSURF.blit(IMAGESDICT['title'],titleRect)
+
+    displayText = BASICFONT.render("Press any key to continue...",True,WHITE,BLACK)
+    displayTextPos = displayText.get_rect()
+    displayTextPos.center = (RESOLUTION[0]//2,topCoord)
+    DISPLAYSURF.blit(displayText,displayTextPos)
+
+    # theme music
+    pygame.mixer.music.load('sounds/theme.mp3')
+    pygame.mixer.music.play(-1,0.0)
+
+    while True: #Main loop for the start screen
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()
+                pygame.mixer.music.stop()
+                return
+        pygame.display.update()
+        FPSCLOCK.tick()
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+if __name__ == '__main__':
+    main()
